a3f7d9dd27e5c4431808c1c8338001ed67563368
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-05-16 18:51:36 +0530

    Fix prepublish script

---
+++ prepublish.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const rmDir = (dirPath) => {
+  if (!fs.existsSync(dirPath)) return;
+  fs.readdirSync(dirPath).forEach((file) => {
+    const filePath = path.join(dirPath, file);
+    if (fs.lstatSync(filePath).isDirectory()) return rmDir(filePath);
+    fs.unlinkSync(filePath);
+  });
+  fs.rmdirSync(dirPath);
+}
+
+['dist', 'types'].forEach((dir) => {
+  rmDir(path.join(__dirname, dir));
+})
