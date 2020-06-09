232188f5fc5c048b38be8a80d4fe7b1d371971ca
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-06-09 16:18:19 +0530

    Fix `importer.ts`

--- src/browser/importer.ts
+++ src/browser/importer.ts
@@ -1,20 +1,19 @@
 export interface Importer {
   All(mod: any): any;
-  Default(mod: any, sourcePath: string): any;
-  Dynamic(toImport: string): any;
-  Member(mod: any, member: string, sourcePath: string): any;
+  Default(mod: any, sourcePath: string, importerPath: string): any;
+  Dynamic(toImport: string, importerPath: string): any;
+  Member(mod: any, member: string, sourcePath: string, importerPath: string): any;
 }
 
 declare const address: string;
-declare const filePath: string;
 
 const importer: Importer = {
   All(mod) {
     if (mod.__cjsModule) return mod.default;
     return mod;
   },
-  Default(mod, sourcePath) {
-    const message = `The requested module "${sourcePath}" does not provide an export named "default". Module imported by "${filePath}"`;
+  Default(mod, sourcePath, importerPath) {
+    const message = `The requested module "${sourcePath}" does not provide an export named "default". Module is imported by "${importerPath}"`;
 
     if (mod.__cjsModule && mod.default.__esModule) {
       if (!('default' in mod.default)) throw new SyntaxError(message);
@@ -23,16 +22,16 @@ const importer: Importer = {
     if (!('default' in mod)) throw new SyntaxError(message);
     return mod.default;
   },
-  async Dynamic(toImport) {
-    const response = await fetch(`${address}/resolve?from=${filePath}&to=${toImport}`);
+  async Dynamic(toImport, importerPath) {
+    const response = await fetch(`${address}/resolve?from=${importerPath}&to=${toImport}`);
     if (!response.ok) {
       throw new TypeError(`[reboost] Failed to resolve dynamically imported module "${toImport}"`);
     }
     const resolvedPath = await response.text();
-    return importer.All(await import(`${address}/transformed?q=${encodeURI(resolvedPath)}&importer=${encodeURI(filePath)}`));
+    return importer.All(await import(`${address}/transformed?q=${encodeURI(resolvedPath)}`));
   },
-  Member(mod, member, sourcePath) {
-    const message = `The requested module "${sourcePath}" does not provide an export named "${member}". Module imported by "${filePath}"`;
+  Member(mod, member, sourcePath, importerPath) {
+    const message = `The requested module "${sourcePath}" does not provide an export named "${member}". Module is imported by "${importerPath}"`;
 
     if (mod.__cjsModule) {
       if (!(member in mod.default)) throw new SyntaxError(message);
