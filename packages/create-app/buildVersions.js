a4392b044388c3121472d0a60ad5057e68e25728
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-07-03 16:25:21 +0530

    Improve `create-app`

---
+++ packages/create-app/buildVersions.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const packagesDir = path.join(__dirname, '../');
+const distDir = path.join(__dirname, 'dist');
+const packageVersions = {};
+
+fs.readdirSync(packagesDir).forEach((dirName) => {
+  // Just in case we store non-directory file in packages dir
+  if (!fs.lstatSync(path.join(packagesDir, dirName)).isDirectory()) return;
+
+  const pkgJSON = JSON.parse(
+    fs.readFileSync(path.join(packagesDir, dirName, 'package.json')).toString()
+  );
+  packageVersions[dirName] = pkgJSON.version;
+});
+
+if (!fs.existsSync(distDir)) fs.mkdirSync(distDir);
+
+fs.writeFileSync(
+  path.join(distDir, './package-versions.json'),
+  JSON.stringify(packageVersions, null, 2)
+);
--- packages/create-app/src/versions.ts
+++ packages/create-app/src/versions.ts
@@ -1,12 +1,19 @@
+import fs from 'fs';
+import path from 'path';
+
+const reboostPackageVersions = JSON.parse(
+  fs.readFileSync(path.join(__dirname, './package-versions.json')).toString()
+);
+
 export const versions = {
-  reboost: '0.x.x',
+  reboost: `^${reboostPackageVersions['core']}`,
   litElement: '^2.3.1',
   electron: '^9.0.4',
   preact: '^10.4.5',
-  reboostPluginSvelte: '0.x.x',
+  reboostPluginSvelte: `^${reboostPackageVersions['plugin-svelte']}`,
   svelte: '^3.23.2',
   react: '^16.13.1',
   reactDom: '^16.13.1',
   vue: '^3.0.0-beta.18',
-  reboostPluginVue: '0.x.x'
+  reboostPluginVue: `^${reboostPackageVersions['plugin-vue']}`
 }
