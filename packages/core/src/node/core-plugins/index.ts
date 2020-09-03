aced167702bcc77e1965cc73f6a38610687bf59b
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-09-03 18:02:54 +0530

    Remove CommonJS interop message

--- packages/core/src/node/core-plugins/index.ts
+++ packages/core/src/node/core-plugins/index.ts
@@ -1,5 +1,3 @@
-import chalk from 'chalk';
-
 import { JSONPlugin } from './json';
 import { LoaderPlugin } from './loader';
 import { ResolverPlugin } from './resolver';
@@ -9,7 +7,6 @@ import { CommonJSMode2Plugin } from './commonjs-mode-2/';
 
 import { ReboostPlugin } from '../index';
 import { getConfig } from '../shared';
-import { tLog } from '../utils';
 
 export const CorePlugins = (): ReboostPlugin[] => {
   const plugins = [
@@ -18,16 +15,6 @@ export const CorePlugins = (): ReboostPlugin[] => {
     ResolverPlugin(),
     NodeEnvPlugin()
   ];
-
-  // TODO: Remove in next release
-  tLog('info', chalk.cyan(
-    'CommonJS interoperability has changed in the last release.\n' +
-    'If your all files are CommonJS modules, your code may not work in this release.\n' +
-    'Please see the configuration docs to fix this issue - ' +
-    'https://github.com/sarsamurmu/reboost/blob/primary/docs/configurations.md#commonjsinterop.\n' +
-    'If your all files are ES modules then just ignore this message.\n' +
-    'Have a great day :)\n'
-  ));
 
   if (getConfig().commonJSInterop.mode > 0) {
     plugins.push(
