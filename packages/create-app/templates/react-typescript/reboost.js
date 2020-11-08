6a26126eca7fa8f6fc27b074c59b2e170e20340d
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-11-08 18:20:55 +0530

    Add React TypeScript template

---
+++ packages/create-app/templates/react-typescript/reboost.js
@@ -0,0 +1,26 @@
+const {
+  start,
+  builtInPlugins: {
+    UsePlugin
+  }
+} = require('reboost');
+const ReactRefreshPlugin = require('@reboost/plugin-react-refresh');
+
+start({
+  entries: [
+    ['./src/index.js', './public/dist/index.js']
+  ],
+  contentServer: {
+    root: './public',
+    open: true
+  },
+  plugins: [
+    UsePlugin({
+      // The following regex enables fast refresh for files
+      // with .js, .ts, .jsx or .tsx extensions
+      // Feel free to use any regex for your files
+      include: /\.[jt]sx?$/i,
+      use: ReactRefreshPlugin()
+    })
+  ]
+});
---
+++ packages/create-app/templates/react-typescript/src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './App.css';
+
+export const App = () => {
+  return (
+    <div className="main">
+      <p>
+        Get started by editing <code>src/App.js</code>
+      </p>
+    </div>
+  )
+}
