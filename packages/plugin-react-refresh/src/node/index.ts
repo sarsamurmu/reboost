33bb4c1db913852d11ac17b915557602adbe0bd2
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-11-15 18:23:24 +0530

    Rework on React Refresh plugin

--- packages/plugin-react-refresh/src/node/index.ts
+++ packages/plugin-react-refresh/src/node/index.ts
@@ -11,61 +11,62 @@ declare namespace ReactRefreshPlugin {
 }
 
 function ReactRefreshPlugin({ excludeNodeModules = true }: ReactRefreshPlugin.Options = {}): ReboostPlugin {
-  let reactRefreshPath: string;
+  const transformOptions: babel.TransformOptions = {
+    ast: false,
+    configFile: false,
+    babelrc: false,
+    envName: 'development',
+    comments: false,
+    compact: true,
+    plugins: [require.resolve('react-refresh/babel')],
+    sourceMaps: true
+  }
+
+  const preCode = /* js */`
+    import * as __Runtime from ${JSON.stringify(path.join(__dirname, '../browser/runtime.js'))};
+    import { hot as ReboostHot } from 'reboost/hot';
+
+    const __prevRefreshReg = window.$RefreshReg$;
+    const __prevRefreshSig = window.$RefreshSig$;
+
+    window.$RefreshReg$ = (type, id) => {
+      const fullId = ReboostHot.id + ' ' + id;
+      __Runtime.register(type, fullId);
+    }
+    window.$RefreshSig$ = __Runtime.createSignatureFunction;\n
+  `;
+
+  const postCode = /* js */`;
+    window.$RefreshReg$ = __prevRefreshReg;
+    window.$RefreshSig$ = __prevRefreshSig;
+
+    ReboostHot.self.accept((updatedModule) => {
+      // Check if all exports are React components
+      if (__Runtime.isReactRefreshBoundary(updatedModule)) {
+        __Runtime.performReactRefresh();
+      } else {
+        ReboostHot.invalidate();
+      }
+    });
+  `;
+
+  const offsetMapping = ';'.repeat(preCode.match(/\n/g).length);
 
   return {
     name: 'react-refresh-plugin',
+    getCacheKey: () => 1,
     async transformJSContent({ code }, filePath) {
       if (excludeNodeModules && /node_modules/.test(filePath)) return;
 
       try {
-        const preCode = /* js */`
-          import * as __Runtime from ${JSON.stringify(path.join(__dirname, '../browser/runtime.js'))};
-          import { hot as ReboostHot } from 'reboost/hot';
-
-          const __prevRefreshReg = window.$RefreshReg$;
-          const __prevRefreshSig = window.$RefreshSig$;
-
-          window.$RefreshReg$ = (type, id) => {
-            const fullId = ReboostHot.id + ' ' + id;
-            __Runtime.register(type, fullId);
-          }
-          window.$RefreshSig$ = __Runtime.createSignatureFunction;\n
-        `;
-
-        const postCode = /* js */`;
-          window.$RefreshReg$ = __prevRefreshReg;
-          window.$RefreshSig$ = __prevRefreshSig;
-
-          ReboostHot.self.accept((updatedModule) => {
-            // Check if all exports are React components
-            if (__Runtime.isReactRefreshBoundary(updatedModule)) {
-              __Runtime.performReactRefresh();
-            } else {
-              ReboostHot.invalidate();
-            }
-          });
-        `;
-
-        const babelResult = await babel.transformAsync(code, {
-          ast: false,
-          configFile: false,
-          babelrc: false,
-          envName: 'development',
-          comments: false,
-          compact: true,
-          plugins: [
-            reactRefreshPath || (reactRefreshPath = require.resolve('react-refresh/babel'))
-          ],
-          sourceMaps: true
-        });
+        const babelResult = await babel.transformAsync(code, transformOptions);
 
         const resultCode = preCode + babelResult.code + postCode;
 
         if (babelResult.map) {
           babelResult.map.sources = [filePath];
           // Fix source map because we are prepend-ing some code which are not mapped
-          babelResult.map.mappings = ';'.repeat(preCode.match(/\n/g).length) + babelResult.map.mappings;
+          babelResult.map.mappings = offsetMapping + babelResult.map.mappings;
         }
 
         return {
