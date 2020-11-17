cf423cbd54739245c47ea8e6fc82e31b221efe0e
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-11-17 17:53:56 +0530

    Update playground code

--- playground/src/vue/App.vue
+++ playground/src/vue/App.vue
@@ -1,21 +1,37 @@
 <template>
-  <p class="msg">{{ message }}</p>
+  <h2>Vue.js Example</h2>
+  <p class="msg">Count is {{ count }}</p>
+  <button v-on:click="increase">Increase</button>
+  <span style="padding: 8px;"></span>
+  <button v-on:click="decrease">Decrease</button>
 </template>
 
 <style scoped>
-.msg {
-  font-family: sans-serif;
-}
+  h2 {
+    font-family: sans-serif;
+    font-weight: normal;
+  }
+
+  .msg {
+    font-family: sans-serif;
+  }
 </style>
 
 <script>
-export default {
-  data() {
-    console.log('Source map test');
-
-    return {
-      message: 'This is working'
+  export default {
+    data() {
+      console.log('Source map test');
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      increase() {
+        this.count++;
+      },
+      decrease() {
+        this.count--;
+      }
     }
   }
-}
 </script>
