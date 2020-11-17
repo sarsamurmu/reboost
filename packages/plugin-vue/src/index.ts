44642d64bea50619fa6fbe4ba0544e6dc633e389
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-11-17 16:34:37 +0530

    Vue Plugin: Add support for Hot Reload

--- packages/plugin-vue/src/index.ts
+++ packages/plugin-vue/src/index.ts
@@ -2,6 +2,8 @@ import * as Compiler from '@vue/compiler-sfc';
 import hashSum from 'hash-sum';
 
 import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
 
 import { ReboostPlugin, RawSourceMap } from 'reboost';
 
@@ -11,10 +13,36 @@ declare namespace VuePlugin {
   }
 }
 
+const md5 = (str: string) => crypto.createHash('md5').update(str).digest('hex');
+const blockHash = (block: Compiler.SFCBlock) => {
+  return block && JSON.stringify(
+    md5(
+      JSON.stringify([
+        ['src', block.src],
+        ['content', block.content],
+        ['attrs', Object.keys(block.attrs).sort().map((key) => [key, block.attrs[key]])]
+      ])
+    )
+  );
+}
+const styleHash = (blocks: Compiler.SFCStyleBlock[]) => {
+  return blocks && blocks.length && JSON.stringify(
+    md5(
+      JSON.stringify(blocks.map((block) => [
+        ['isModule', !!block.module],
+        ['vars', block.attrs.vars],
+        ['isScoped', !!block.scoped],
+      ]))
+    )
+  );
+}
+
 function VuePlugin(options: VuePlugin.Options = {}): ReboostPlugin {
   let compiler = Compiler;
 
   if (options.compiler) compiler = options.compiler;
+
+  // TODO: Fix issue with multiple source map when multiple style blocks are used
 
   return {
     name: 'vue-plugin',
@@ -30,10 +58,6 @@ function VuePlugin(options: VuePlugin.Options = {}): ReboostPlugin {
         const id = hashSum(this.rootRelative(filePath));
 
         let preCode = 'const __modExp = {}';
-        const postCode = `
-          __modExp.render = render;
-          export default __modExp;
-        `;
 
         if (descriptor.script) {
           preCode = descriptor.script.content.replace('export default', 'const __modExp =');
@@ -42,10 +66,9 @@ function VuePlugin(options: VuePlugin.Options = {}): ReboostPlugin {
         let cssStr = '';
         let cssMap: RawSourceMap;
         let hasScopedCSS = false;
-        const promises: Promise<any>[] = [];
 
-        descriptor.styles.forEach((styleBlock) => {
-          promises.push((async () => {
+        await Promise.all(
+          descriptor.styles.map(async (styleBlock) => {
             if (!hasScopedCSS) hasScopedCSS = styleBlock.scoped;
 
             const result = await compiler.compileStyleAsync({
@@ -55,16 +78,28 @@ function VuePlugin(options: VuePlugin.Options = {}): ReboostPlugin {
               scoped: styleBlock.scoped,
               modules: !!styleBlock.module,
               preprocessLang: styleBlock.lang as any,
+              postcssOptions: {
+                from: filePath,
+                to: filePath,
+                map: {
+                  inline: false,
+                  annotation: false
+                }
+              }
             });
 
+            if (result.map) {
+              result.map.sources.forEach((source, idx) => {
+                if (source === path.basename(filePath)) result.map.sources[idx] = filePath;
+              });
+            }
+
             cssStr += result.code;
-            cssMap = await this.mergeSourceMaps(cssMap, styleBlock.map as any);
-          })());
-        });
+            cssMap = await this.mergeSourceMaps(styleBlock.map as any, result.map as any)
+          })
+        );
 
-        await Promise.all(promises);
-
-        const compiled = compiler.compileTemplate({
+        const template = compiler.compileTemplate({
           filename: filePath,
           compilerOptions: {
             scopeId: hasScopedCSS ? `data-v-${id}` : null
@@ -73,17 +108,68 @@ function VuePlugin(options: VuePlugin.Options = {}): ReboostPlugin {
           preprocessLang: descriptor.template.lang
         });
 
-        cssMap = this.getCompatibleSourceMap(cssMap);
-        cssStr += '\n\n' + this.getSourceMapComment(cssMap);
+        cssStr += '\n\n' + this.getSourceMapComment(
+          this.getCompatibleSourceMap(cssMap)
+        );
 
-        const CSSCode = `
-          const styleTag = document.createElement('style');
-          styleTag.innerHTML = ${JSON.stringify(cssStr)};
-          document.head.appendChild(styleTag);
+        const postCode = `
+          __modExp.render = render;
+
+          import { hot as ReboostHot } from 'reboost/hot';
+
+          let __hmrData = {
+            hash: {
+              script: ${blockHash(descriptor.script)},
+              scriptSetup: ${blockHash(descriptor.scriptSetup)},
+              template: ${blockHash(descriptor.template)},
+              style: ${styleHash(descriptor.styles)}
+            },
+            style: ${JSON.stringify(cssStr)},
+          }
+          let __styleEl;
+          export const __HMR_DATA__ = __hmrData;
+
+          // NOTE: This enables HMR in Vue internally
+          __modExp.__hmrId = ReboostHot.id;
+
+          if (!ReboostHot.data) {
+            __styleEl = document.createElement('style');
+            __styleEl.textContent = __hmrData.style;
+            document.head.appendChild(__styleEl);
+          }
+
+          if (!ReboostHot.data && __VUE_HMR_RUNTIME__) {
+            __VUE_HMR_RUNTIME__.createRecord(ReboostHot.id, __modExp);
+
+            ReboostHot.self.accept((updatedModule) => {
+              const component = updatedModule.default;
+              const newHMRData = updatedModule.__HMR_DATA__;
+              const curHash = __hmrData.hash;
+              const newHash = newHMRData.hash;
+
+              if (
+                curHash.script !== newHash.script ||
+                curHash.scriptSetup !== newHash.scriptSetup ||
+                curHash.style !== newHash.style
+              ) {
+                __VUE_HMR_RUNTIME__.reload(ReboostHot.id, component);
+              } else if (curHash.template !== newHash.template) {
+                __VUE_HMR_RUNTIME__.rerender(ReboostHot.id, component.render);
+              }
+
+              if (__hmrData.style !== newHMRData.style) {
+                __styleEl.textContent = newHMRData.style;
+              }
+
+              __hmrData = newHMRData;
+            });
+          }
+
+          export default __modExp;
         `;
 
         return {
-          code: `${preCode};${compiled.code};${CSSCode};${postCode};`,
+          code: `${preCode};${template.code};${postCode};`,
           map: descriptor.script && descriptor.script.map as any,
           type: 'js',
         }
