2fd554aa3ad2a06cb778921ba9aec644137acd31
Sarsa Murmu <sarsamurmu05@gmail.com>
2020-11-20 14:47:31 +0530

    Update playground code [ci skip]

--- playground/src/postcss/index.js
+++ playground/src/postcss/index.js
@@ -1,8 +1,9 @@
 import './base.css';
+import './tailwind.css';
 
 document.body.append(...new DOMParser().parseFromString(/* html */`
   <p>This should be large</p>
   <div><p>This should be small</p></div>
   <br><br>
-  <span class="pink-box">Pink Box</span>
+  <span class="blue-button">Pink Box</span>
 `, 'text/html').body.children);
